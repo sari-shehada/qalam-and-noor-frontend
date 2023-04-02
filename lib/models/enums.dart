205d251a40dd0c1977abb894f5ddf757e9050cc6
Sari Shehada <sarishehada@gmail.com>
2023-04-02 23:01:52 +0300

    Added maps to reflect enums for religion and educational status

--- lib/models/enums.dart
+++ lib/models/enums.dart
@@ -5,7 +5,15 @@ enum Religion {
   undefined,
 }
 
+Map<Religion, String> religionAsString = {
+  Religion.islam: 'إسلام',
+  Religion.christianity: 'المسيحية',
+  Religion.other: 'أخرى',
+  Religion.undefined: 'غير محدد',
+};
+
 enum EducationalStatus {
+  none,
   basic,
   middle,
   secondary,
@@ -13,3 +21,13 @@ enum EducationalStatus {
   master,
   doctorate,
 }
+
+Map<EducationalStatus, String> educationalStatusAsString = {
+  EducationalStatus.none: 'لا يوجد',
+  EducationalStatus.basic: 'أساسي',
+  EducationalStatus.middle: 'اعدادي',
+  EducationalStatus.secondary: 'ثانوي',
+  EducationalStatus.bachelor: 'بكالوريوس',
+  EducationalStatus.master: 'ماجستير',
+  EducationalStatus.doctorate: 'دكتوراه',
+};
