40e60b750572e3dc677cd0be29e56b6a4fbbff96
Mostafa talb <darkdivel1997@gmail.com>
2023-04-03 01:49:14 +0300

    added 4 modules from erd

---
+++ lib/models/exam.dart
@@ -0,0 +1,8 @@
+class Exam{
+  int id;
+  int classId;
+  int percentage;
+  Enum examType;
+}
+
+//TODO:make enum for exam type
---
+++ lib/models/semester.dart
@@ -0,0 +1,6 @@
+class Semester{
+  int id;
+  int yearRecordId;
+  Enum type;
+}
+//TODO : make enum for semester type
