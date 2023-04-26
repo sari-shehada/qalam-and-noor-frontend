52c7b234130fa1e29aaba49e0fa30342afb4d831
Mostafa talb <darkdivel1997@gmail.com>
2023-04-26 08:18:30 +0300

    Added Father,Mother and Responsible-Person DB Helpers

---
+++ lib/models/helpers/database_helpers/mother_db_helper.dart
@@ -0,0 +1,58 @@
+import 'package:kalam_noor/models/agendas/mother.dart';
+import 'package:kalam_noor/tools/logic_tools/crud_interface.dart';
+
+import '../../../tools/logic_tools/network_service.dart';
+
+class MotherDBHelper implements CRUDInterface<Mother> {
+  String get _controllerName => 'MotherController/';
+  static MotherDBHelper get instance => MotherDBHelper();
+  @override
+  Future<List<Mother>> getAll() async {
+    String url = '${_controllerName}GetMothers';
+    List<Mother> allMothers = await HttpService.getParsed<List<Mother>, List>(
+      url: url,
+      dataMapper: (parsedData) {
+        return parsedData.map(
+          (e) {
+            return Mother.fromMap(e);
+          },
+        ).toList();
+      },
+    );
+    return allMothers;
+  }
+
+  @override
+  Future<Mother?> getById(int id) async {
+    String url = '${_controllerName}GetMotherById?id=$id';
+    Mother? mother = await HttpService.getParsed<Mother?, Map<String, dynamic>>(
+      url: url,
+      dataMapper: (responseData) {
+        return Mother.fromMap(responseData);
+      },
+    );
+    return mother;
+  }
+
+  Future<int> getMothersCount() async {
+    return await getAll().then((value) => value.length);
+  }
+
+  @override
+  Future<Mother> insert(Mother object) async {
+    String url = '${_controllerName}InsertMother';
+    int? result =
+        await HttpService.post(url: url, serializedBody: object.toJson());
+    if (result == null) return throw Exception();
+    return object.copyWith(id: result);
+  }
+
+  @override
+  Future<bool> update(Mother object) async {
+    String url = '${_controllerName}UpdateMother';
+    int? result =
+        await HttpService.post(url: url, serializedBody: object.toJson());
+    if (result == null) return false;
+    return result == 1;
+  }
+}
---
+++ lib/models/helpers/father_db_helper.dart
@@ -0,0 +1,58 @@
+import 'package:kalam_noor/models/agendas/father.dart';
+import 'package:kalam_noor/tools/logic_tools/crud_interface.dart';
+import '../../tools/logic_tools/network_service.dart';
+
+class FatherDBhelper implements CRUDInterface<Father> {
+  String get _controllerName => 'FatherController/';
+  static FatherDBhelper get instance => FatherDBhelper();
+
+  @override
+  Future<List<Father>> getAll() async {
+    String url = '${_controllerName}GetCities';
+    List<Father> allFathers = await HttpService.getParsed<List<Father>, List>(
+      url: url,
+      dataMapper: (parsedData) {
+        return parsedData.map(
+          (e) {
+            return Father.fromMap(e);
+          },
+        ).toList();
+      },
+    );
+    return allFathers;
+  }
+
+  @override
+  Future<Father?> getById(int id) async {
+    String url = '${_controllerName}GetFatherById?id=$id';
+    Father? father = await HttpService.getParsed<Father?, Map<String, dynamic>>(
+      url: url,
+      dataMapper: (responseData) {
+        return Father.fromMap(responseData);
+      },
+    );
+    return father;
+  }
+
+  Future<int> getFathersCount() async {
+    return await getAll().then((value) => value.length);
+  }
+
+  @override
+  Future<Father> insert(Father object) async {
+    String url = '${_controllerName}InsertFather';
+    int? result =
+        await HttpService.post(url: url, serializedBody: object.toJson());
+    if (result == null) return throw Exception();
+    return object.copyWith(id: result);
+  }
+
+  @override
+  Future<bool> update(Father object) async {
+    String url = '${_controllerName}UpdateFather';
+    int? result =
+        await HttpService.post(url: url, serializedBody: object.toJson());
+    if (result == null) return false;
+    return result == 1;
+  }
+}
