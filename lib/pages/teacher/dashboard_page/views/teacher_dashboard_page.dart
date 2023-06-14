57fd5ebdb366d2ec6bd24d457fbee935038ed8ef
Mostafa talb <darkdivel1997@gmail.com>
2023-06-14 23:34:21 +0300

    started working on teacher page

--- lib/pages/teacher/dashboard_page/views/teacher_dashboard_page.dart
+++ lib/pages/teacher/dashboard_page/views/teacher_dashboard_page.dart
@@ -1,24 +1,25 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import '../../../../configs/fonts.dart';
-import '../../../../tools/ui_tools/buttons.dart';
 import '../../../../tools/ui_tools/custom_appbar.dart';
 import '../../../../tools/ui_tools/custom_scaffold.dart';
-import '../../../../tools/ui_tools/ui_tools.dart';
-
-import '../../../../controllers/account_controller.dart';
+import '../../../../tools/widgets/employee_dashboard_profile_widget.dart';
+import '../controllers/teacher_dashboard_controller.dart';
 
 class TeacherDashboardPage extends StatelessWidget {
   const TeacherDashboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TeacherDashboardControllerController controller = Get.put(
+      TeacherDashboardControllerController(),
+    );
     return CustomScaffold(
       appBar: const CustomAppBar(
         title: 'الرئيسية',
         iconData: FontAwesomeIcons.house,
+        actionButton: EmployeeDashboardProfileWidget(),
       ),
       body: Center(
         child: Column(
@@ -26,11 +27,6 @@ class TeacherDashboardPage extends StatelessWidget {
             Text(
               'صفحة المدرس',
               style: ProjectFonts.headlineMedium(),
-            ),
-            AddVerticalSpacing(value: 40.h),
-            CustomOutlinedButton(
-              onTap: () => Get.find<AccountController>().logout(),
-              child: 'تسجيل الخروج',
             ),
           ],
         ),
